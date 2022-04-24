84b9f0c8f485d2f097a1e1dbae1d5802d8ca77bf
abaialihan <abai.alihan@gmail.com>
2022-04-24 19:26:51 +0600

    add UserController

--- iStore/src/main/java/com/marsh/iStore/model/Role.java
+++ iStore/src/main/java/com/marsh/iStore/model/Role.java
@@ -3,8 +3,7 @@ package com.marsh.iStore.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-   USER;
-
+   USER, ADMIN;
 
    @Override
    public String getAuthority() {
